deb0ff96c524086bde6b83ca5f3621385733c3b7
camillageber <camilla.geber@gmail.com>
2023-02-28 15:51:28 -0300

    feat: implemented 'challenge_study_schedule'

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui"""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time):
+    counter = 0
+    try:
+        for login, logout in permanence_period:
+            if login <= target_time <= logout:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
