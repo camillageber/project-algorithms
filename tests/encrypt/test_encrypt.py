de64e3d3e1bfce10f60046861ad85044415abd47
camillageber <camilla.geber@gmail.com>
2023-02-28 22:14:13 -0300

    test: implemented 'test_encrypt_message'

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("pipoca", 9) == "acopip"
+
+    assert encrypt_message("pipoca", -1) == "acopip"
+
+    assert encrypt_message("pipoca", 2) == "acop_ip"
+
+    assert encrypt_message("chocolate", 4) == "etalo_cohc"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("hello", "world")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(00, 00)
